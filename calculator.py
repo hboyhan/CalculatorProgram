f2c746a03e5b838559494a705c57966488d419d2
Harvey Boyhan <harveyboyhan23@gmail.com>
2025-03-07 10:21:33 +0000

    Created a function to delay the print speed by creating a delay between characters. Also imported the time module to use the sleep function.

--- calculator.py
+++ calculator.py
@@ -1,12 +1,20 @@
-# Imported modules
+# Imported Modules
 import sys
+import time
 
-# Welcome message for the user
+# Delayed print function, reduces the speed of the print function by creating a delay between each character
+def delayed_print(message, delay=1):
+    for char in message:
+        print(char, end='', flush=True)
+        time.sleep(delay) 
+    print()
+
+# Welcome message for the user, uses the delayed print function
 def Welcome_Message():
-    print("Welcome to the calculator program \n" \
+    delayed_print("Welcome to the calculator program \n" \
     "Type in your calculation, ensuring spaces between all numbers and operators, then press enter \n" \
     "Example: 1 + 1 \n" \
-    "Type 'exit' to close the program \n")
+    "Type 'exit' to close the program \n", delay=0.05)
 
 # User input function, takes in the user's calculation and returns it, if the user types 'exit' the program will close
 def User_Input():
@@ -15,7 +23,7 @@ def User_Input():
         sys.exit()
     else:
         return calc
-    
+
 # Function to split the user's calculation into a list, then cast all numbers as integers while leaving the operators as strings
 def Split_Cast_Calculation(calculation):
     split_calc = calculation.split(" ")
@@ -58,7 +66,9 @@ def BODMAS(calc):
             case int:
                 continue
         return calc
-    
+# CURRENT ISSUE - The program is not calculating the correct answer if the equation is more then 2 numbers, it is only calculating the first two numbers in the list, Cause: The for loop is skipping over certain operators as the list has been modified and the index is no longer correct. Solution: Ask Sartak for help.
+
+
 # Function to use the user input, split and cast, and BODMAS functions to run the calculator program
 def Running():
     Welcome_Message()
